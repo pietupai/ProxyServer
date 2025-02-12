d9c128c426152d5644522598c4b2b5b198b6e8c2
pietupai <pietupai@gmail.com>
2025-02-12 14:26:04 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// config.js
+module.exports = {
+  allowedOrigins: [
+    'https://your-allowed-origin1.com',
+    'https://your-allowed-origin2.com'
+    // 'https://another-allowed-origin.com' // Temporarily commented out
+  ],
+  disallowedOrigins: [
+    'https://your-disallowed-origin1.com',
+    'https://your-disallowed-origin2.com'
+  ],
+  allowedDestinations: [
+    'https://www.google.com',
+    'https://www.example.com'
+  ],
+  disallowedDestinations: [
+    'https://www.disallowed-destination.com',
+    'https://www.malicious-site.com'
+  ]
+};
