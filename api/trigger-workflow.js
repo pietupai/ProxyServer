8afabc115c35632b760bcfe861ae7efa6ae351dc
pietupai <pietupai@gmail.com>
2025-02-12 11:52:38 +0200

    Update trigger-workflow.js

--- api/trigger-workflow.js
+++ api/trigger-workflow.js
@@ -8,9 +8,6 @@ module.exports = async (req, res) => {
 
     if (req.method === 'OPTIONS') {
         // Handle preflight request
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return res.status(204).end();
     }
 
