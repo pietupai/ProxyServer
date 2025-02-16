cf866c41961c428e68a5cb9d24667a405a9a96fe
pietupai <pietupai@gmail.com>
2025-02-16 07:31:12 +0200

    Update sse-server.js

--- api/sse-server.js
+++ api/sse-server.js
@@ -38,7 +38,7 @@ app.get('/api/events', (req, res) => {
     //const intervalId = setInterval(sendServerTime, 10000); // 10 sekunnin välein
 
     //const keepAlive = setInterval(() => {  res.write('data: keep-alive\n\n');  console.log('Keep-alive message sent');  }, 5000);
-    setTimeout(() => { res.write('data: keep-alive\n\n');  console.log('Keep-alive message sent');  }, 5000);
+    setTimeout(() => { console.log('Keep-alive message sent'); res.write('data: keep-alive\n\n'); }, 5000);
 
     req.on('close', () => {
         console.log('SSE connection closed ');
