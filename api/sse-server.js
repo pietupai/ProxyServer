6b20db9e6c5a47ff5fdef41f7f1f62f60dc0e45c
pietupai <pietupai@gmail.com>
2025-02-16 07:48:43 +0200

    Update sse-server.js

--- api/sse-server.js
+++ api/sse-server.js
@@ -24,6 +24,7 @@ app.get('/api/events', (req, res) => {
 
     const previous_now = DateTime.now().setZone('Europe/Helsinki').toLocaleString(DateTime.TIME_WITH_SECONDS);
     console.log('SSE connection established : ${previous_now}');
+    console.log(`SSE connection established : ${previous_now}`);
     let previousTime = Date.now();
 
     const sendServerTime = () => {
