17d0bb0020323a4c31cf15403e3dba0c99d6923f
pietupai <pietupai@gmail.com>
2025-02-17 16:04:39 +0200

    Update sse-server.js

--- api/sse-server.js
+++ api/sse-server.js
@@ -3,6 +3,8 @@ const { DateTime } = require('luxon');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const isVercel = !!process.env.VERCEL;
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,8 +16,18 @@ app.use((req, res, next) => {
     next();
 });
 
-let lastSentTime = Date.now();
+let lastSentTime = process.env.LAST_SENT_TIME ? parseInt(process.env.LAST_SENT_TIME) : Date.now();
+console.log(`Initial lastSentTime: ${new Date(lastSentTime).toLocaleString()}`);
+
 let checkInterval;
+
+const updateLastSentTime = (timestamp) => {
+    if (isVercel) {
+        process.env.LAST_SENT_TIME = timestamp.toString();
+    } else {
+        lastSentTime = timestamp;
+    }
+};
 
 const sendServerTime = (res, updateTimestamp = true) => {
     const currentTime = Date.now();
@@ -24,10 +36,15 @@ const sendServerTime = (res, updateTimestamp = true) => {
     const message = `Server time: ${now} - elapsed: ${elapsed}s`;
     res.write(`data: ${message}\n\n`);
     if (updateTimestamp) {
-        lastSentTime = currentTime; // Päivitetään lähetysaika vain tarvittaessa
+        updateLastSentTime(currentTime); // Päivitetään lähetysaika vain tarvittaessa
     }
     console.log('SSE message sent:', message);
 };
+
+app.listen(port, () => {
+    console.log(`SSE server running on http://localhost:${port}`);
+    console.log(isVercel ? "Running on Vercel" : "Running locally");
+});
 
 app.get('/api/events', (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
@@ -47,7 +64,6 @@ app.get('/api/events', (req, res) => {
     if (elapsedReconnect >= 30) {
         sendServerTime(res);
     } else {
-        // Jos viimeisestä viestistä ei ole kulunut 30 sekuntia, odotetaan jäljellä oleva aika ja lähetetään sitten
         const timeoutId = setTimeout(() => {
             sendServerTime(res);
             checkInterval = setInterval(() => {
@@ -81,8 +97,4 @@ app.get('/api/events', (req, res) => {
         console.log('SSE connection closed');
         clearInterval(checkInterval);
     });
-});
-
-app.listen(port, () => {
-    console.log(`SSE server running on http://localhost:${port}`);
 });
