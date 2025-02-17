8ad342fc90d128ec2339640e0e5c58fb48958204
pietupai <pietupai@gmail.com>
2025-02-17 16:36:02 +0200

    Update sse-server.js

--- api/sse-server.js
+++ api/sse-server.js
@@ -3,8 +3,6 @@ const { DateTime } = require('luxon');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const isVercel = !!process.env.VERCEL;
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,22 +14,10 @@ app.use((req, res, next) => {
     next();
 });
 
-if (isVercel && !process.env.LAST_SENT_TIME) {
-    process.env.LAST_SENT_TIME = Date.now().toString();
-}
-
-let lastSentTime = process.env.LAST_SENT_TIME ? parseInt(process.env.LAST_SENT_TIME) : Date.now();
+let lastSentTime = Date.now();
 console.log(`Initial lastSentTime: ${new Date(lastSentTime).toLocaleString()}`);
 
 let checkInterval;
-
-const updateLastSentTime = (timestamp) => {
-    if (isVercel) {
-        process.env.LAST_SENT_TIME = timestamp.toString();
-    } else {
-        lastSentTime = timestamp;
-    }
-};
 
 const sendServerTime = (res, updateTimestamp = true) => {
     const currentTime = Date.now();
@@ -40,14 +26,13 @@ const sendServerTime = (res, updateTimestamp = true) => {
     const message = `Server time: ${now} - elapsed: ${elapsed}s`;
     res.write(`data: ${message}\n\n`);
     if (updateTimestamp) {
-        updateLastSentTime(currentTime); // Päivitetään lähetysaika vain tarvittaessa
+        lastSentTime = currentTime; // Päivitetään lähetysaika vain tarvittaessa
     }
     console.log('SSE message sent:', message);
 };
 
 app.listen(port, () => {
     console.log(`SSE server running on http://localhost:${port}`);
-    console.log(isVercel ? "Running on Vercel" : "Running locally");
 });
 
 app.get('/api/events', (req, res) => {
@@ -70,7 +55,7 @@ app.get('/api/events', (req, res) => {
             sendServerTime(res);
         } else {
             const timeoutId = setTimeout(() => {
-                sendServerTime(res);
+                sendServerTime(res, false); // Päivitetään lähetysaika vain, jos viesti lähetetään
                 checkInterval = setInterval(() => {
                     const currentTime = Date.now();
                     const elapsed = (currentTime - lastSentTime) / 1000;
