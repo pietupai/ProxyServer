eb4515cbb21225b540e45ac4a70fb829469be120
pietupai <pietupai@gmail.com>
2025-02-21 00:23:36 +0200

    Update sse.js

--- api/sse.js
+++ api/sse.js
@@ -1,6 +1,4 @@
-let sseClients = [];
-
-const addSseClient = (req, res) => {
+const addSseClient = (req, res, clients) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache, no-transform');
   res.setHeader('Connection', 'keep-alive');
@@ -14,36 +12,22 @@ const addSseClient = (req, res) => {
   };
 
   res.on('close', () => {
-    sseClients = sseClients.filter(client => client.id !== clientId);
+    clients = clients.filter(client => client.id !== clientId);
     console.log(`SSE client disconnected. Client ID: ${clientId}`);
   });
 
+  clients.push(newClient);
   console.log('SSE client connected. Client ID:', clientId);
-  return newClient;
+  return clients;
 };
 
-const sendSseMessage = (data) => {
-  console.log('Sending SSE message to', sseClients.length, 'clients');
-  sseClients.forEach(client => {
+const sendSseMessage = (clients, data) => {
+  console.log('Sending SSE message to', clients.length, 'clients');
+  clients.forEach(client => {
     client.res.write(`data: ${data}\n\n`);
     client.res.flush(); // Pakotetaan lähettämään tiedot
     console.log('SSE message sent to client', client.id);
   });
 };
 
-// Oletusvientifunktio vaaditaan
-module.exports = (req, res) => {
-  if (req.method === 'GET') {
-    console.log('SSE connection request received');
-    const newClient = addSseClient(req, res);
-    sseClients.push(newClient);
-    console.log('Total SSE clients:', sseClients.length);
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-};
-
-// Export the sendSseMessage function and sseClients array so it can be used in webhook.js
-module.exports.sendSseMessage = sendSseMessage;
-module.exports.sseClients = sseClients;
+module.exports = { addSseClient, sendSseMessage };
