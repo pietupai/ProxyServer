b2ebea139bc51adbabc1c54920b02798643427ab
pietupai <pietupai@gmail.com>
2025-02-21 00:50:39 +0200

    Update sse.js

--- api/sse.js
+++ api/sse.js
@@ -1,10 +1,8 @@
-let sseClients = [];
-
-const addSseClient = (req, res) => {
+const addSseClient = (req, res, clients) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache, no-transform');
   res.setHeader('Connection', 'keep-alive');
-  res.setHeader('X-Accel-Buffering', 'no'); // Ensure no buffering
+  res.setHeader('X-Accel-Buffering', 'no'); // Varmistaakseen, ettei vastausta puskeroida
   res.flushHeaders();
 
   const clientId = Date.now();
@@ -14,38 +12,24 @@ const addSseClient = (req, res) => {
   };
 
   res.on('close', () => {
-    const index = sseClients.findIndex(client => client.id === clientId);
+    const index = clients.findIndex(client => client.id === clientId);
     if (index !== -1) {
-      sseClients.splice(index, 1);
+      clients.splice(index, 1);
     }
     console.log(`SSE client disconnected. Client ID: ${clientId}`);
   });
 
-  sseClients.push(newClient);
+  clients.push(newClient);
   console.log('SSE client connected. Client ID:', clientId);
 };
 
-const sendSseMessage = (data) => {
-  console.log('Sending SSE message to', sseClients.length, 'clients');
-  sseClients.forEach(client => {
+const sendSseMessage = (clients, data) => {
+  console.log('Sending SSE message to', clients.length, 'clients');
+  clients.forEach(client => {
     client.res.write(`data: ${data}\n\n`);
     client.res.flush(); // Pakotetaan lähettämään tiedot
     console.log('SSE message sent to client', client.id);
   });
 };
 
-// Oletusvientifunktio
-module.exports = (req, res) => {
-  if (req.method === 'GET') {
-    console.log('SSE connection request received');
-    addSseClient(req, res);
-    console.log('Total SSE clients:', sseClients.length);
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-};
-
-// Export the functions so they can be used in webhook.js
-module.exports.addSseClient = addSseClient;
-module.exports.sendSseMessage = sendSseMessage;
+module.exports = { addSseClient, sendSseMessage };
