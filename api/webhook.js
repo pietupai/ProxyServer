5f5110d470f19a79370a09f68077d163a0b91863
pietupai <pietupai@gmail.com>
2025-02-20 17:30:15 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -5,30 +5,30 @@ const events = require('events');
 const fetch = require('node-fetch');
 
 const app = express();
-//app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(cors());
 
 const eventEmitter = new events.EventEmitter();
 
+function getCurrentTimestamp() {
+  return new Date().toISOString();
+}
+
 app.post('/api/webhook', async (req, res) => {
   try {
-    //console.log(req);
-    //console.log(req.headers);
     const body = req.body;
-    console.log('Webhook event received:', body);
+    console.log(`[${getCurrentTimestamp()}] Webhook event received:`, body);
 
     // Fetch the updated response.txt content
     const response = await fetch('https://raw.githubusercontent.com/pietupai/hae/main/response.txt');
     const data = await response.text();
 
     // Emit event with the updated content
-    console.log(`[${getCurrentTimestamp()}] Emitting newWebhook event with data:`, data);
     eventEmitter.emit('newWebhook', data);
 
     res.status(200).send(data);
   } catch (error) {
-    console.error('Error handling webhook:', error);
+    console.error(`[${getCurrentTimestamp()}] Error handling webhook:`, error);
     res.status(500).send('Error handling webhook');
   }
 });
@@ -40,25 +40,24 @@ app.get('/api/sse', (req, res) => {
   res.setHeader('Connection', 'keep-alive');
   res.flushHeaders();
 
-  console.log('SSE connection established');
+  console.log(`[${getCurrentTimestamp()}] SSE connection established`);
 
   const listener = (data) => {
-    console.log('Sending data to SSE client:', data);
+    console.log(`[${getCurrentTimestamp()}] Sending data to SSE client:`, data);
     res.write(`data: ${data}\n\n`);
   };
 
   eventEmitter.on('newWebhook', listener);
 
   req.on('close', () => {
-    clearInterval(keepAlive);
     eventEmitter.removeListener('newWebhook', listener);
-    console.log('SSE connection closed');
+    console.log(`[${getCurrentTimestamp()}] SSE connection closed`);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`[${getCurrentTimestamp()}] Server is running on port ${PORT}`);
 });
 
 module.exports = app;
